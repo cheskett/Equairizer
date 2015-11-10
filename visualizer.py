9d03396067ee98b6b82b4d8bdb8eb7ffe5d15b52
Cameron Heskett <cameron517@gmail.com>
2015-11-09 23:27:10 -0500

    modularized audio parser in visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -6,80 +6,88 @@ import pyaudio
 import threading
 import numpy as np
 from math import sqrt
-from Adafruit_PWM_Servo_Driver import PWM
+#from Adafruit_PWM_Servo_Driver import PWM
 
 filename = sys.argv[1]
 chunk = 500
 lock = threading.Lock()
 num_bands = 9
-pwm = PWM(0x40)
+#pwm = PWM(0x40)
 p = pyaudio.PyAudio()
 
-if __name__ == '__main__':
 
-# Open the wave file and get info
-    wave_file = wave.open(filename, 'rb')
-    data_size = wave_file.getnframes()
-    sample_rate = wave_file.getframerate()
-    sample_width = wave_file.getsampwidth()
-    duration = data_size / float(sample_rate)
+class AudioParser:
+    
+    def __init__(self, filename, num_bands=None):
+        self.filename = filename
+        self.wave_file = wave.open(filename, 'rb')
+        self.data_size = self.wave_file.getnframes()
+        self.sample_rate = self.wave_file.getframerate()
+        self.sample_width = self.wave_file.getsampwidth()
+        self.duration = self.data_size / float(self.sample_rate)
+        
+        if num_bands is None:
+            self.num_bands = 9
+        else:
+            self.num_bands = num_bands
+        self.fft_averages = []
+        self.stream = p.open(format =
+                p.get_format_from_width(self.wave_file.getsampwidth()),
+                channels = self.wave_file.getnchannels(),
+                rate = self.wave_file.getframerate(),
+                output = True)
 
-    fft_averages = []
-    stream = p.open(format =
-            p.get_format_from_width(wave_file.getsampwidth()),
-            channels = wave_file.getnchannels(),
-            rate = wave_file.getframerate(),
-            output = True)
+        #Calculate FFT informatuon
+        self.fouriers_per_second = 44.1 # Frames per second
+        self.fourier_spread = 1.0/self.fouriers_per_second
+        self.fourier_width = self.fourier_spread
+        self.fourier_width_index = self.fourier_width * float(self.sample_rate)
+        self.sample_size = self.fourier_width_index
+    
+        if len(sys.argv) < 3:
+            self.length_to_process = int(self.duration)-1
+        else:
+            self.length_to_process = float(sys.argv[2])
 
-    #Calculate FFT informatuon
-    fouriers_per_second = 44.1 # Frames per second
-    fourier_spread = 1.0/fouriers_per_second
-    fourier_width = fourier_spread
-    fourier_width_index = fourier_width * float(sample_rate)
-    sample_size = fourier_width_index
-    if len(sys.argv) < 3:
-        length_to_process = int(duration)-1
-    else:
-        length_to_process = float(sys.argv[2])
+        self.total_transforms = int(round(self.length_to_process * self.fouriers_per_second))
+        self.fourier_spacing = round(self.fourier_spread * float(self.sample_rate))
+        
+        
+    def getBandWidth(self):
+        return (2.0/self.sample_size) * (self.sample_rate / 2.0)
 
-    total_transforms = int(round(length_to_process * fouriers_per_second))
-    fourier_spacing = round(fourier_spread * float(sample_rate))
-
-    def getBandWidth():
-        return (2.0/sample_size) * (sample_rate / 2.0)
-
-    def freqToIndex(f):
+    def freqToIndex(self, f):
         # If f (frequency is lower than the bandwidth of spectrum[0]
-        if f < getBandWidth()/2:
+        if f < self.getBandWidth()/2:
             return 0
-        if f > (sample_rate / 2) - (getBandWidth() / 2):
-            return sample_size -1
-        fraction = float(f) / float(sample_rate)
-        index = round(sample_size * fraction)
+        if f > (self.sample_rate / 2) - (self.getBandWidth() / 2):
+            return self.sample_size -1
+        fraction = float(f) / float(self.sample_rate)
+        index = round(self.sample_size * fraction)
         return index
 
 
-    def average_fft_bands(fft_array):
-        del fft_averages[:]
-        for band in range(0, num_bands):
+    def average_fft_bands(self, fft_array):
+        del self.fft_averages[:]
+        for band in range(0, self.num_bands):
             avg = 0.0
             if band == 0:
                 lowFreq = int(0)
             else:
-                lowFreq = int(int(sample_rate / 2) / float(2 ** (num_bands - band)))
-            hiFreq = int((sample_rate / 2) / float(2 ** ((num_bands-1) - band)))
-            lowBound = int(freqToIndex(lowFreq))
-            hiBound = int(freqToIndex(hiFreq))
+                lowFreq = int(int(self.sample_rate / 2) / float(2 ** (self.num_bands - band)))
+            hiFreq = int((self.sample_rate / 2) / float(2 ** ((self.num_bands-1) - band)))
+            lowBound = int(self.freqToIndex(lowFreq))
+            hiBound = int(self.freqToIndex(hiFreq))
             for j in range(lowBound, hiBound):
                 #avg += (fft_array[j])**2 / (hiBound - lowBound + 1)
                 avg += (fft_array[j])
 
             avg /= (hiBound - lowBound + 1)
             #avg = np.sqrt(avg)
-            fft_averages.append(avg)
-        return fft_averages
+            self.fft_averages.append(avg)
+        return self.fft_averages
 
-    def scale_values(old_val):
+    def scale_values(self, old_val):
         if old_val > 2000:
             old_val = 2000
         old_val = int(old_val)
@@ -91,45 +99,46 @@ if __name__ == '__main__':
         print new_val
         return int(new_val)
 
-    def process_sample(sample_range):
+    def process_sample(self, sample_range):
         fft_data = abs(np.fft.fft(sample_range))
-        fft_data *= ((2**.5)/sample_size)
-        fft_averages = average_fft_bands(fft_data)
+        fft_data *= ((2**.5)/self.sample_size)
+        self.fft_averages = self.average_fft_bands(fft_data)
         output = ""
-        pwm.setPWM(0,0,scale_values(fft_averages[4]))
-        for num in fft_averages:
+        #pwm.setPWM(0,0,scale_values(fft_averages[4]))
+        for num in self.fft_averages:
             output = output + " " + str(int(num))
         lock.release()
         print output
 
 
-    def begin():
-        print "Sample rate: %s" % sample_rate
-        print "Data Size: %s" % data_size
-        print "Sample width: %s" % sample_width
-        print "Duration: %s" % duration
-        print "For Fourier width of "+str(fourier_width)+" need "+str(sample_size)+" samples each FFT"
-        print "Doing "+str(fouriers_per_second)+" Fouriers per second"
-        print "Total " + str(total_transforms * fourier_spread)
-        print "Spacing: "+str(fourier_spacing)
-        print "Total transforms "+str(total_transforms)
+    def begin(self):
+        print "Sample rate: %s" % self.sample_rate
+        print "Data Size: %s" % self.data_size
+        print "Sample width: %s" % self.sample_width
+        print "Duration: %s" % self.duration
+        print "For Fourier width of "+str(self.fourier_width)+" need "+str(self.sample_size)+" samples each FFT"
+        print "Doing "+str(self.fouriers_per_second)+" Fouriers per second"
+        print "Total " + str(self.total_transforms * self.fourier_spread)
+        print "Spacing: "+str(self.fourier_spacing)
+        print "Total transforms "+str(self.total_transforms)
 
-        data = wave_file.readframes(chunk)
+        data = self.wave_file.readframes(chunk)
         current_sample = ''
 
         while data != '':
-            stream.write(data)
+            self.stream.write(data)
             current_sample = current_sample + data
-            if len(current_sample) == sample_size * 2:
+            if len(current_sample) == self.sample_size * 2:
                 pack_fmt = '%dh' % (len(current_sample) / 2)
                 sound_data = struct.unpack(pack_fmt, current_sample)
                 lock.acquire()
-                work_thread = threading.Thread(target=process_sample,args=(sound_data,))
+                work_thread = threading.Thread(target=self.process_sample,args=(sound_data,))
                 work_thread.start()
                 current_sample=''
-            data = wave_file.readframes(chunk)
+            data = self.wave_file.readframes(chunk)
 
-        wave_file.close()
+        self.wave_file.close()
 
-    begin()
+player = AudioParser(filename)
+player.begin()
 
