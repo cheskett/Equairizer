bf3e782067adc6fe0f74aaffc69b308874070b57
Christopher wirt <cwirt@purdue.edu>
2015-12-01 21:58:21 +0000

    Uploading and some CSS

--- Equairizer.py
+++ Equairizer.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, url_for
 import os
 from werkzeug import secure_filename
 
@@ -17,13 +17,24 @@ def hello_world():
 def upload_page():
     return render_template('upload_song.html')
 
-@app.route('/upload_song',methods=['GET','POST'])
-def process_upload
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
+@app.route('/upload_song',methods=['POST'])
+def process_upload():
+	file = request.files['file']
+	filename = secure_filename(file.filename)
+	ext = filename.rsplit('.', 1)[len(filename.rsplit('.', 1))-1]
+	if file and ext in ALLOWED_EXTENSIONS:
+		print "Gottem!"
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		if ext == "wav":
+			return render_template('upload_success.html')
+		elif ext == "mp3":
+			#ffmpeg
+			return render_template('upload_success.html')
+		else:
+			return render_template('upload_failure.html')
+	else:
+		print "Nope.."
+		return render_template('upload_song.html')
 
 
 if __name__ == '__main__':
